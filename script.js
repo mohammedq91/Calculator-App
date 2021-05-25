6e393035bfe73be8c32ea04157da6f2066988cd4
Mohammed <mohammedq91@gmail.com>
2021-05-24 22:22:51 -0400

    displaying numbers & operands on inputDisplay

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-var nonOperationalBtns = document.getElementsByClassName('nonOperationalButtons')
-const operationalsButtons = document.getElementById('operationalButtons')
-const clear = document.getElementById('clearButton')
-const digitsDisplay = document.getElementById('digitsDisplay')
+const numbers = document.getElementsByClassName('number')
+const dot = document.getElementsByClassName('dot');
+const operators = document.getElementsByClassName('operator')
+const clear = document.getElementById('clear').addEventListener('click', reset)
+const inputDisplay = document.getElementById('inputDisplay')
 
-
-nonOperationalBtns.addEventListener('click',function(e){
+function reset(e){
   e.preventDefault();
-  console.log('you click me!')
-  // digitsDisplay.display = nonOperationalBtns.value
+  inputDisplay.value ="0"
+  console.log('Hello')
+}
 
-});
+for (let i=0; i < numbers.length; i++){
+  const button = numbers[i];
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
+    inputDisplay.value = button.textContent;
+  });
 
+  function add(){
+    inputDisplay.value= button.textContent 
+  }    
+  add();
+
+};
+
+for (let i=0; i< operators.length; i++){
+  const operator = operators[i];
+
+  operator.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('you clicked me!')
+    inputDisplay.value = operator.textContent
+  })
+}
 
 
 
