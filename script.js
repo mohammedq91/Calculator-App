70378367e35c3aaaef4787ee439b5fe8258ebf31
Mohammed <mohammedq91@gmail.com>
2021-05-26 19:31:15 -0400

    calculating two numbers

--- script.js
+++ script.js
@@ -4,36 +4,55 @@ const operators = document.getElementsByClassName('operator')
 const clear = document.getElementById('clear').addEventListener('click', reset)
 const inputDisplay = document.getElementById('inputDisplay')
 
+let firstOperand = '';
+let secondOperand = '';
+let operatorValue = '';
+
 function reset(e){
   e.preventDefault();
   inputDisplay.value ="0"
-  console.log('Hello')
 }
 
 for (let i=0; i < numbers.length; i++){
   const number = numbers[i];
   number.addEventListener('click', function(e) {
     e.preventDefault();
-    inputDisplay.value = number.textContent;
+
+    if (!firstOperand){
+      firstOperand = number.textContent;
+      console.log('firstOperand: ',firstOperand);
+      inputDisplay.value = number.textContent;
+    }
+    if (firstOperand && operatorValue) {
+      secondOperand = number.textContent;
+      console.log('secondOperand: ',secondOperand);
+      inputDisplay.value = number.textContent;
+    }
   });
-
-  function add(){
-    inputDisplay.value= number.textContent 
-  }    
-  add();
-
 };
 
-for (let i=0; i< operators.length; i++){
+for (let i=0; i < operators.length; i++){
   const operator = operators[i];
 
   operator.addEventListener('click', function(e){
     e.preventDefault();
-    console.log('you clicked me!')
-    inputDisplay.value = operator.textContent
+
+    if (firstOperand && !operatorValue){
+      operatorValue = operator.textContent;
+      console.log('operator: ', operatorValue)
+      inputDisplay.value = operator.textContent
+    }
+    
   })
 }
 
+
+function calculate(firstOperand, operator, secondOperand){
+  switch (firstOperand) {
+    case add:
+      return firstOperand + SecondOperand;
+  };
+};
 
 
 
