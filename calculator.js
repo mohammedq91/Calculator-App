192e75f8c1c49294b107c1066b9fffd283d48d20
Mohammed <mohammedq91@gmail.com>
2021-05-28 20:40:21 -0400

    Activate calculate function

--- calculator.js
+++ calculator.js
@@ -1,18 +1,17 @@
-const clear = document.getElementById('clear').addEventListener('click', clearBtn)
+const clearBtn = document.getElementById('clearBtn').addEventListener('click', reset)
 const numbers = document.getElementsByClassName('number')
 const dot = document.getElementsByClassName('dot');
 const operators = document.getElementsByClassName('operator')
 const display = document.getElementById('display')
 const equalsButton = document.querySelector('.equals')
+display.value ="0"
 
 let firstOperand = '';
 let secondOperand = '';
 let operatorValue = '';
 
 
-
-
-function clearBtn(e){
+function reset(e){
   e.preventDefault();
   display.value ="0"
 }
@@ -47,37 +46,46 @@ for (let i=0; i < operators.length; i++){
     }
     console.log('operator type: ', operatorValue)
     display.value = operator.textContent
-    
   })
-
-  function calculate(operators){
-    if (operatorValue === '+'){
-      return firstOperand + secondOperand
-      // return firstOperand + operatorValue + secondOperand;
-    };
-    
-    // if (operatorValue === '-'){
-    //   return firstOperand - secondOperand;
-    // };
-    // if (operatorValue === '/'){
-    //   return firstOperand / secondOperand;
-    // };
-    // if (operatorValue === 'x'){
-    //   return firstOperand * secondOperand;
-    // };
-  };
-
 }
 
 
+function calculate(){
+  if (operatorValue === '+'){
+    //what to return? a number or a string?
+    return Number(firstOperand)+Number(secondOperand);
+  }; 
 
+  if (operatorValue === '-'){
+    return Number(firstOperand)-Number(secondOperand);
+  };
+  
+  if (operatorValue === '/'){
+    return Number(firstOperand)/Number(secondOperand);
+  };
+
+  if (operatorValue === 'x'){
+    return Number(firstOperand)*Number(secondOperand);
+  };
+};
 
 equalsButton.addEventListener('click', ()=>{
-  // const result = calculate();
-  // console.log(result)
-  console.log('equals')
-  calculate(firstOperand, secondOperand);
-})
+  console.log('Home')
+  //what conditions should I check before calculating
+  if (!firstOperand && !operatorValue && !secondOperand){
+    const result = calculate();
+    console.log(result)
+    console.log("Hello world!")
+  } else if (!operatorValue && !secondOperand){
+    const result = calculate();
+    console.log(result)
+    console.log('You clicked me!!')
+
+  }
+  //how to display the result?
+  display.value = result.value
+  // what to reset?
+});
 
 
 
