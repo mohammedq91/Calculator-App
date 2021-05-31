55c6e2c10aad2ee5a738656ac34ce4feb4c1ba79
Mohammed <mohammedq91@gmail.com>
2021-05-30 22:35:48 -0400

    Completed continuous functionality

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
-const clearBtn = document.getElementById('clearBtn').addEventListener('click', reset)
 const numbers = document.getElementsByClassName('number')
-const dot = document.getElementsByClassName('dot');
 const operators = document.getElementsByClassName('operator')
+const decimal = document.getElementsByClassName('dot')
+const clearBtn = document.getElementById('clearBtn').addEventListener('click', reset)
 const display = document.getElementById('display')
 const equalsButton = document.querySelector('.equals')
 display.value ="0"
@@ -9,6 +9,8 @@ display.value ="0"
 let firstOperand = '';
 let secondOperand = '';
 let operatorValue = '';
+let outcome;
+let decimalNumber = '.';
 
 function reset(e){
   e.preventDefault();
@@ -18,21 +20,46 @@ function reset(e){
   operatorValue = '';
 }
 
+for (i=0; i < decimal.length; i++){
+  const decimalNum = decimal[i];
+  decimalNum.addEventListener('click',function(e){
+    e.preventDefault();
+
+    if (!decimalNumber){
+      
+    }
+
+    if (!firstOperand){
+      firstOperand = 0. + Number(firstOperand);
+      console.log('firstOperand is: ', firstOperand)
+    }; 
+
+    if (firstOperand && operatorValue){
+      secondOperand = 0. + Number(secondOperand);
+      console.log('secondOperand is:', secondOperand)
+    };
+
+  });
+};
+
+
 for (let i=0; i < numbers.length; i++){
   const number = numbers[i];
   number.addEventListener('click', function(e) {
     e.preventDefault();
 
-    if (!firstOperand){
+    if (!firstOperand){  
+    //if there isn't 1st operand 
+    //and number button/element was pressed then-->
       firstOperand = number.textContent;
+      console.log('firstOperand is: ', firstOperand)
+
     }
     if (firstOperand && operatorValue) {
       secondOperand = number.textContent;
-    }
-    console.log('firstOperand: ',firstOperand);
-    console.log('secondOperand: ',secondOperand);
+      console.log('secondOperand is:', secondOperand)
+    };
     display.value = number.textContent;
-
 
   });
 };
@@ -44,39 +71,54 @@ for (let i=0; i < operators.length; i++){
     e.preventDefault();
 
     if (firstOperand && !operatorValue){
+      //if there was 1st operand and was NOT operator
+      // and operator button was pressed then -->
       operatorValue = operator.textContent;
-    }
-    console.log('operator type: ', operatorValue)
-    display.value = operator.textContent
-  })
-}
+    };
+
+    if (firstOperand && operatorValue && secondOperand){ 
+      // continuous functionality/operations.
+      //if there were 1st Operand && 2nd Operand && operator
+      // and operator button was pressed then -->
+      outcome = calculate();
+      display.value = outcome;
+      firstOperand = outcome;
+      secondOperand = ''
+    };
+  });
+  
+};
 
 
 function calculate(){
-  let outcome;
   if (operatorValue === '+'){
-    //what to return? a number or a string?
     outcome = Number(firstOperand) + Number(secondOperand);
-    display.value = outcome;
+    console.log('the outcome is: ',outcome)
+    return outcome;
   }; 
 
   if (operatorValue === '-'){
     outcome = Number(firstOperand) - Number(secondOperand);
-    display.value = outcome
+    return outcome
   };
   
   if (operatorValue === '/'){
     outcome = Number(firstOperand) / Number(secondOperand);
-    display.value = outcome
+    return outcome
   };
 
   if (operatorValue === 'x'){
     outcome = Number(firstOperand) * Number(secondOperand);
-    display.value = outcome
+    return outcome
   };
 };
 
 equalsButton.addEventListener('click', ()=>{
-  calculate();
+  //What condidtions to check before calculating?
+  if(firstOperand && operatorValue && secondOperand){
+    display.value = calculate();
+    firstOperand = outcome;
+    secondOperand = ''
+  };
 });
 
